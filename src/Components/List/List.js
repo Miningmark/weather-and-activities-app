ad98e42712f5bf4a7f97bb24b1d82c13fbb44ea8
Krischan-Klug <krischanklug@icloud.com>
2024-03-01 12:34:41 +0100

    finished liftUp-state

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,28 +1,19 @@
 import ListItem from "../ListItem/ListItem";
-import { useState } from "react";
-import { uid } from "uid";
 import "./List.css";
 
-export default function List({}) {
-  const [activities, setActivities] = useState([
-    { name: "wdwadw", isForGoodWeather: true, id: uid() },
-    { name: "xyz", isForGoodWeather: false, id: uid() },
-    { name: "abc", isForGoodWeather: true, id: uid() },
-    { name: "123", isForGoodWeather: false, id: uid() },
-    { name: "test", isForGoodWeather: true, id: uid() },
-    { name: "example", isForGoodWeather: false, id: uid() },
-    { name: "coding", isForGoodWeather: true, id: uid() },
-    { name: "hiking", isForGoodWeather: false, id: uid() },
-    { name: "reading", isForGoodWeather: true, id: uid() },
-    { name: "playing", isForGoodWeather: true, id: uid() },
-  ]);
-  const [isGoodWeather, setIsGoodWeather] = useState(false);
+export default function List({ activities, weatherData, onDeleteActivity }) {
   return (
     <ul className="activitiesList">
       {activities
-        .filter((activity) => isGoodWeather === activity.isForGoodWeather)
+        .filter(
+          (activity) => weatherData.isGoodWeather === activity.isForGoodWeather
+        )
         .map((activity) => (
-          <ListItem key={activity.id} activity={activity}></ListItem>
+          <ListItem
+            key={activity.id}
+            activity={activity}
+            onDeleteActivity={onDeleteActivity}
+          ></ListItem>
         ))}
     </ul>
   );
